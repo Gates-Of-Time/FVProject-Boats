1818fca3dfcecad50ee04b3f38ea85d1b74181e2
Randy Girard <randy@tradepending.com>
2023-07-29 17:32:54 -0400

    Dont auto load these. need to do manually

--- sql/load_boats.sql
+++ sql/load_boats.sql
@@ -1,9 +1,9 @@
-source butcher.sql
-source oot.sql
-source freporte.sql
-source erudnext.sql
-source erudsxing.sql
-source qeynos.sql
+-- source butcher.sql
+-- source oot.sql
+-- source freporte.sql
+-- source erudnext.sql
+-- source erudsxing.sql
+-- source qeynos.sql
 
 UPDATE npc_types set runspeed = 3.33973515123456 WHERE id IN (10137, 68009, 69001); -- Butcher <=> Freeport
 UPDATE npc_types set runspeed = 2.3917 WHERE id IN (24501, 98501, 1501); -- Qeynos <=> Erudin
