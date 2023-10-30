dbf6e4fe446bc7ba84aa7f55a655f410342a9039
Randy Girard <randy@tradepending.com>
2023-10-30 11:30:14 -0400

    Update load_boats.sql
    Wasn't actually wrong

--- sql/load_boats.sql
+++ sql/load_boats.sql
@@ -5,6 +5,6 @@
 -- source erudsxing.sql
 -- source qeynos.sql
 
-UPDATE npc_types set runspeed = 2.3917 WHERE id IN (10137, 68009, 69001); -- Butcher <=> Freeport
-UPDATE npc_types set runspeed = 3.33973515123456 WHERE id IN (24501, 98501, 1501); -- Qeynos <=> Erudin
+UPDATE npc_types set runspeed = 3.33973515123456 WHERE id IN (10137, 68009, 69001); -- Butcher <=> Freeport
+UPDATE npc_types set runspeed = 2.3917 WHERE id IN (24501, 98501, 1501); -- Qeynos <=> Erudin
 INSERT INTO content_flags(flag_name, enabled, notes) VALUES ('boats', true, 'Enables the boats system.');
